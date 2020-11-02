be8f14a62b6a43e665072324576edebb2f365f8d
dgabin <depairegabin@macbook-pro.home>
2020-11-02 16:02:00 +0100

    ajout tp8

--- scripts/mariadb.sql
+++ scripts/mariadb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE "test";
 CREATE USER 'root'@'%' IDENTIFIED BY 'password';
 GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'root'@'%';
 GRANT SELECT ON performance_schema.* TO 'root'@'%';
+FLUSH PRIVILEGES;
--- scripts/master-user.sql
+++
@@ -1,7 +0,0 @@
-mariadb -uroot -ppassword
-
-CREATE USER IF NOT EXISTS 'replicant'@'%' identified by 'password';
-
-grant replication slave on *.* to replicant;
-
-flush privileges;
